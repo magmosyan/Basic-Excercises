774bc65a8f2fc52d4f404eb4418595cf0b946b28
magmosyan <69057554+magmosyan@users.noreply.github.com>
2025-05-10 14:59:26 +0400

    Add files via upload

---
+++ 1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Margarita";
+            int age = 30;
+            float height= 1.65f;
+            bool isStudent = false;
+
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+            Console.WriteLine(height);
+            Console.WriteLine(isStudent);
+
+
+
+        }
+    }
+}
